b177f2db2a5c0b5a7d109bad144644e36693e0d0
liangy <liangy@msoe.edu>
2019-03-29 02:42:13 -0500

    Implemented the crop algorithm

--- seperation.py
+++ seperation.py
@@ -2,65 +2,62 @@
 """
 Created on Mon Oct 15 09:18:40 2018
 
-@author: jpansh
+@author: liangy
 """
+
 
 import os
 import cv2
 import numpy as np
-#from threading import Thread
-#import matplotlib.image as mpimg
-
-input_height = 512  # 768
-input_width = 512  # 1024
-rate = 4
-sub_rate = 2
-
-# Limit the size of the message to be within the max_size
-def resize_content_img(img):
-    return cv2.resize(img, dsize=(input_height, input_width), interpolation=cv2.INTER_AREA)
+import sys
+import time
+import random
 
 
-def read_image(path):
-    _img_ = cv2.imread(path)
-    return _img_.astype(np.float32)
+def crop_imgs(img):
+    croped = []
+    # Resize to 256 * 256 img
+    resized_img = cv2.resize(img, (256, 256))
+    # Crop 224 * 224 pieces
+    crop_num = 4
+    for _ in range(crop_num):
+        start_point_y = random.randint(0, 32)
+        start_point_x = random.randint(0, 32)
+        croped_img = resized_img[start_point_y:start_point_y + 224,
+                                 start_point_x:start_point_x + 224]
+        croped.append(croped_img)
+    return croped
 
 
-def load_content_img(path):
-    _img_ = read_image(path)
-    _img_ = resize_content_img(_img_)
-    # show_img = img.astype(np.uint8)
-    # plt.imshow(show_img)
-    # plt.show()
-    return _img_
-
-def main():
-    base = 'output/color/'
-    output = 'output/color_sepe/'
-    folders = os.listdir(base)
-
-    for folder in folders:
-        if folder != 'Inand wash painting' :
-            files = os.listdir(base + folder + '/')
-            for num, file in enumerate(files):
-                img = load_content_img(base+folder+'/'+file)
-#                img *= (255.0/img.max())
-                height,width,_ = img.shape
-                for index in range(1,rate+1):
-                    file_name = file.split('.')[0] + str(index) + '.jpg'
-                    if index == 1:
-                        new_img = img[0:int(height/32*31),0:int(width/32*31),:]
-                    elif index == 2:
-                        new_img = img[int(height/32*1):,:int(width/32*31),:]
-                    elif index == 3:
-                       new_img = img[:int(height/32*31),int(width/32*1):,:]
-                    elif index == 4:
-                        new_img = img[int(height/32*1):,int(width/32*1):,:]
-                    cv2.imwrite(output + folder + '/' + file_name,  new_img)
-                    print('write ' + (output + folder + '/' + file_name))
+def main(argv):
+    output = 'cropedImages'
+    if not os.path.exists(output):
+        os.makedirs(output)
+    styles = os.listdir(argv[1])
+    for style in styles:
+        if style != ".DS_Store":
+            # Create the directory
+            if not os.path.exists(output + "/" + style):
+                os.makedirs(output + "/" + style)
+            # Get imgs for each style
+            imgs = os.listdir(argv[1] + "/" + style)
+            for img in imgs:
+                if img != ".DS_Store":
+                    img_data = cv2.imread(argv[1] + "/" + style + "/" + img)
+                    croped_imgs = crop_imgs(img_data)
+                    for n, croped_img in enumerate(croped_imgs):
+                        file_name = output + "/" + style + "/" + img[:-4] + "_" + str(n) + ".png"
+                        cv2.imwrite(file_name, croped_img)
+            break
 
 
 if __name__ == '__main__':
-    print('starting making dataset...')
-    main()
-    print('finished making dataset')
+    if len(sys.argv) < 2:
+        print("Program accepts the directory parameter!")
+        sys.exit(1)
+
+    print("start separation...")
+    start_time = time.time()
+    main(sys.argv)
+    end_time = time.time()
+    print('finished:' + str(end_time - start_time) + "s")
