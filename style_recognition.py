5494ff3287b9445c0449b0389250a67276283230
liangy <liangy@msoe.edu>
2019-04-01 20:33:16 -0500

    Added GPU support

--- style_recognition.py
+++ style_recognition.py
@@ -382,7 +382,7 @@ def main(argv):
 
     # The counter for tracking the number of batches
     count = 0
-    with tf.Session() as sess:
+    with tf.device('/gpu:0'), tf.Session() as sess:
 
         sess.run(tf.global_variables_initializer())
         sess.run(tf.local_variables_initializer())
