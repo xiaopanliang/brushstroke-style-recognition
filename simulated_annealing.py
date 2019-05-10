07a3f279fb94dfd745dac998087175e55dcb9e60
Josiah Yoder <yoder@msoe.edu>
2019-05-10 15:31:45 -0500

    Simulated annealing that works (by making smaller images)

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,13 +1,25 @@
 from random import randint, uniform
+# pip install opencv-python
 import cv2
 import math
 import os
 import numpy as np
 
+SOURCE_IMAGE = "test.png" #.jpg"
+NEW_DIMENSIONS = 30
+
+# Source of test.jpg:
+# abidin-dino_drawing-pain-1968
 
 dirName = "tmp_tests/"
-k_max = 100000000000
+# k_max = 100000000000 # Value received from Yahui and Shaoyan
+k_max = 500_000_000
 
+# WRITE_PERIOD = 1000000 # Value received from Yahui and Shaoyan
+WRITE_PERIOD = 5_000_000
+REPORT_PERIOD = 1_000_000
+
+AVG_DIFF_DECAY_RATE = 1 / REPORT_PERIOD
 
 def temperature(r):
     return 1 - r
@@ -47,6 +59,9 @@ def difference(pt, ns, img):
     total = 0
     for n in ns:
         n_px = img[n[1]][n[0]]
+
+        p_px = [float(x) for x in p_px]
+
         diff_r = math.sqrt(
             math.pow(p_px[0] - n_px[0], 2) +
             math.pow(p_px[1] - n_px[1], 2) +
@@ -66,9 +81,11 @@ def P(orig, new):
 
 
 def simulated_annealing(img):
+    avg_diff = None
     height, width, _ = img.shape
     img.flags.writeable = True
-    for k in range(0, k_max):
+
+    for k in range(0, k_max+1):
 
         p1 = random_pick_pt(height, width)
         p2 = random_pick_pt(height, width)
@@ -79,19 +96,26 @@ def simulated_annealing(img):
         orig = difference(p1, n1, img) + difference(p2, n2, img)
         new = difference(p1, n2, img) + difference(p2, n1, img)
 
-        if P(orig, new) > 0:
+        diff = orig - new
+        if avg_diff is None:
+            avg_diff = diff
+        else:
+            avg_diff = avg_diff * (1-AVG_DIFF_DECAY_RATE) + AVG_DIFF_DECAY_RATE * diff
+
+        if P(orig, new) > 0: # or uniform(0,1) < math.exp(-2*k/k_max):
             swap(p1, p2, img)
-        if k % 5000 == 0:
-            print(k)
-        if k % 1000000 == 0:
-            cv2.imwrite(dirName + "test_result_" + str(k) + ".jpg", img)
+        if k % REPORT_PERIOD == 0:
+            print('k:',k,'avg_diff:',avg_diff,'orig:',orig)
+        if k % WRITE_PERIOD == 0:
+            cv2.imwrite(dirName + "test_result_" + str(k) + ".png", img)
 
     return img
 
 
 if not os.path.exists(dirName):
     os.makedirs(dirName)
-img = cv2.imread("test.jpg")
+img = cv2.imread(dirName + "/" + SOURCE_IMAGE)
+img = cv2.resize(img, (NEW_DIMENSIONS, NEW_DIMENSIONS))
 
 # Reshape the image and shuffle pixels
 height, width, depth = img.shape
@@ -102,4 +126,4 @@ np.random.shuffle(img)
 img = np.reshape(img, [height, width, depth])
 
 img = simulated_annealing(img)
-cv2.imwrite("test_result.jpg", img)
+cv2.imwrite(dirName + "test_result.png", img)
